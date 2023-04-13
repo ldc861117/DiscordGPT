10c84a53d9121581e60d9098db34151c0639fcda
ldc861117 <66586980+ldc861117@users.noreply.github.com>
2023-04-13 07:36:15 +0000

    updated to use wss

--- bot.js
+++ bot.js
@@ -1,19 +1,61 @@
+const fetch = require("node-fetch");
+const WebSocket = require("ws");
 // Initialize the Discord bot client
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-// Listen for the 'ready' event and log a message
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const ws = new WebSocket("wss://chatapi.andylyu.com/chat");
+let requestId = 0;
+let messageQueue = {};
 
-// Listen for the 'message' event and call your backend
-client.on('message', async message => {
-  const text = message.content
-  const response = await fetch('localhost/chat:12345', { body: { text } })
-  const data = await response.json()
-  message.channel.send(data.message)
-})
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+
+  const text = message.content;
+  const request = {
+    id: requestId++,
+    object: "text",
+    created: Date.now(),
+    text: text,
+    model: "gpt-3.5-turbo-0301",
+  };
+  messageQueue[request.id] = {
+    originalMessage: message,
+    chunks: [],
+  };
+
+  ws.send(JSON.stringify(request));
+});
+
+ws.on("message", (data) => {
+  const message = JSON.parse(data);
+
+  if (message.object === "chat.completion.chunk") {
+    const choices = message.choices;
+    if (choices && choices.length > 0) {
+      const lastChoice = choices[choices.length - 1];
+      const id = message.id;
+      messageQueue[id].chunks.push(lastChoice.delta.content);
+
+      if (lastChoice.finish_reason === "stop") {
+        const botResponse = messageQueue[id].chunks.join("");
+        const originalMessage = messageQueue[id].originalMessage;
+        originalMessage.channel.send(botResponse);
+        delete messageQueue[id];
+      }
+    }
+  } else if (message === "[DONE]") {
+    ws.close();
+  }
+});
+
+ws.on("close", () => {
+  console.log("WebSocket closed");
+});
 
 // Connect the bot to Discord using your bot token
 client.login('MTA5NTk1MzAxNDgzNTEzODYyMA.GglmMS.EMHVojF7O_gUMyS_xht42kB9XfOcz-ARffEGNg')
