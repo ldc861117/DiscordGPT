3f5eb22d761f67c4a1df3c5f0ddb42f0d631f070
ldc861117 <66586980+ldc861117@users.noreply.github.com>
2023-04-14 08:26:13 +0000

    update

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-require('dotenv/config');
-const axios = require('axios');
-const { Client, IntentsBitField } = require('discord.js')
-
+require("dotenv/config");
+const EventSource = require('eventsource');
+const axios = require("axios");
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -11,57 +11,66 @@ const client = new Client({
   ],
 });
 
-client.on('ready', () => {
-  console.log('The bot is online!');
+client.on("ready", () => {
+  console.log("The bot is online!");
 });
 
 client.on("messageCreate", async (message) => {
-  // Create a console log for each message
   console.log(
     `Message received: ${message.content} from ${message.author.username}`
   );
   if (message.author.bot) return;
-  if (message.channel.id !== process.env.CHANNEL_ID) return;
-  if (message.content.startsWith("!")) return;
 
   let conversationLog = [
     { role: "system", content: "You are a friendly chatbot." },
   ];
+  conversationLog.push({ role: "user", content: message.content });
+  await message.channel.sendTyping();
+  //const backendUrl = "http://chatapi.andylyu.com/chat";
 
-
-
-  conversationLog.push({
-    role: "user",
-    content: msg.content,
-  });
-
-
-
-    const backendUrl = "http://chatapi.andylyu.com/chat"; // Replace with your backend URL
-
-    try {
-      // Start axios request to backend print to console
-      console.log("Sending request to backend...");
-      const response = await axios.post(backendUrl, {
-        messages: conversationLog,
+  try {
+    async function fetchReply(conversationLog) {
+      const response = await axios.post('http://chatapi.andylyu.com/chat', {
+        headers: { 'Content-Type': 'application/json' },
+        data: JSON.stringify({ messages: conversationLog }),
       });
-
-      const data = response.data;
-      let botResponse = "";
-
-      data.forEach((item) => {
-        if (item.choices && item.choices[0].delta.content) {
-          botResponse += item.choices[0].delta.content;
-          // Print to console
-          console.log(`Bot response: ${botResponse}`);
+    
+      let botReply = '';
+      const stream = new ReadableStream({
+        start(controller) {
+          axios.interceptors.response.use((response) => {
+            if (typeof response.data === 'string') {
+              response.data.split('\n').forEach((line) => {
+                controller.enqueue(line);
+              });
+            }
+            return response;
+          });
+        },
+      });
+    
+      const reader = stream.getReader();
+      let result = await reader.read();
+      while (!result.done) {
+        const value = result.value;
+        if (value.startsWith('data: ')) {
+          const data = value.slice(6);
+          if (data === '[DONE]') {
+            break;
+          } else {
+            botReply += data;
+          }
         }
-      });
-
-      message.reply(botResponse);
-    } catch (error) {
-      console.error(`Backend ERR: ${error.message}`);
+        result = await reader.read();
+      }
+      return botReply;
     }
-});
 
+    const result = fetchReply(conversationLog);
+    message.reply(result);
+  } catch (error) {
+    console.error(`Backend ERR: ${error.message}`);
+  }
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
